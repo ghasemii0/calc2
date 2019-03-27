09847e5f37468b78d753e0edbb06fea5f8153782
Brian Hensley and Mathew Akre <mathew@>
2019-03-27 15:15:32 -0700

    add functionallity to subtract, multiple, and divide; plus styling

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,25 @@ $(document).ready(function() {
     $("#output").text(result);
     // Business (or back-end) logic:
   });
+  $("form#subtract").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
 });
-
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-// var result = add(number1, number2);
-// alert(result);
