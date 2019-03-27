0a0a289924c340d3b7e5ab8cf312eda8056ef5d9
Brian Hensley and Mathew Akre <brian@>
2019-03-27 14:17:52 -0700

    add var result, gets rid of alerts and add output, plues notes on frontend and backend

--- js/scripts.js
+++ js/scripts.js
@@ -13,15 +13,15 @@ var multiply = function(number1, number2) {
 var divide = function(number1, number2) {
   return number1 / number2;
 };
-
+// User interface (or front-end) logic:
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
-    alert(add(number1, number2));
-
-    // console.log(add(number1, number2));
+    var result = add(number1, number2);
+    $("#output").text(result);
+    // Business (or back-end) logic:
   });
 });
 
